973c05be547489f6c392111ddf4f3b1cd018dbfb
franco@mumuki.org <franco@mumuki.org>
2018-05-16 14:00:56 -0400

    Mumuki Export on 2018-05-16 14:00:56 -0400

--- 00004_Tipando funciones/default.hs
+++ 00004_Tipando funciones/default.hs
@@ -1,5 +1,5 @@
 esPar :: -- completar acá
-esPar unNumero = (mod unNumero 2) == 0
+esPar unNumero = (rem unNumero 2) == 0
 
 cantidadDeLetras :: -- completar acá
 cantidadDeLetras unaPalabra = length unaPalabra
