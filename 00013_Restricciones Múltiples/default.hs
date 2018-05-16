2a326f9af81934acf15608507499631687fd6ccb
franco@mumuki.org <franco@mumuki.org>
2018-05-16 15:07:16 -0400

    Mumuki Export on 2018-05-16 15:07:16 -0400

--- 00013_Restricciones Múltiples/default.hs
+++ 00013_Restricciones Múltiples/default.hs
@@ -1,2 +1,5 @@
 -- completá el tipo acá
 estaEntre valor menor mayor = valor >= menor && valor <= mayor
+
+-- completá el tipo acá
+positivoOEstaEntre unValor otroValor menor mayor numero = unValor > 0 || estaEntre otroValor menor mayor 
