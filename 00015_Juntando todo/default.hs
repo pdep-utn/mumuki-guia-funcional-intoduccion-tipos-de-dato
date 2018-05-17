4c069da51b3a2ec9ae18cfc2702be40e58e45e1c
franco@mumuki.org <franco@mumuki.org>
2018-05-17 10:02:53 -0400

    Mumuki Export on 2018-05-17 10:02:53 -0400

---
+++ 00015_Juntando todo/default.hs
@@ -0,0 +1,8 @@
+-- completá acá
+ignoraElPrimero primero segundo = segundo
+
+-- completá acá
+sumarTres uno otro otroMas = uno + otro + otroMas
+
+-- compararSi
+compararSi condicion uno otro = not condicion || uno == otro
