5c55b2be25200a20e6330c90bced98dfbd16ff31
felipecalvo239@gmail.com <felipecalvo239@gmail.com>
2019-04-07 23:42:34 +0000

    Mumuki Export on 2019-04-07 23:42:34 +0000

--- 00013_Restricciones múltiples/test.hs
+++
@@ -1,8 +0,0 @@
-it "estaEntre funciona con enteros" $ do 
-  estaEntre (4::Int) 5 6 `shouldBe` False
-
-it "estaEntre funciona con flotantes" $ do 
-  estaEntre (4::Float) 1 6 `shouldBe` True
-
-it "estaEntre funciona con caracteres" $ do 
-  estaEntre 'g' 'a' 'z' `shouldBe` True 
